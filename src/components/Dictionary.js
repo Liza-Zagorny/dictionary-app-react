0739f459cdf77375b2c2118ab6b2eeb08ed8fa88
Liza Neiman <5gb.place.for.love@gmail.com>
2022-08-15 12:19:39 +0300

    added axios

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
 import "../css/Dictionary.css";
+import axios from "axios";
 
 function Dictionary() {
-  const [word, setWord] = useState(null);
+  const [word, setWord] = useState(undefined);
 
   function handleChange(event) {
     setWord(event.target.value);
   }
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${word}`);
+    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    axios.get(url).then(handleResponse);
   }
+
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
@@ -19,7 +27,6 @@ function Dictionary() {
           className="form-control"
           autoFocus
           autoComplete="off"
-          value={word}
           onChange={handleChange}
         />
         <button type="submit" className="btn btn-primary btn-sm">
