11ff3930e3e2f76c8e1fa9b23c1276d6abb0dc05
Liza Neiman <5gb.place.for.love@gmail.com>
2022-08-15 11:44:40 +0300

    basic search engine

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/Dictionary.css";
 
 function Dictionary() {
+  const [word, setWord] = useState(null);
+
+  function handleChange(event) {
+    setWord(event.target.value);
+  }
   function search(event) {
     event.preventDefault();
-    alert("Searching");
+    alert(`Searching for ${word}`);
   }
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" />
-        <button type="submit" className="btn btn-primary">
+        <input
+          type="search"
+          className="form-control"
+          autoFocus
+          autoComplete="off"
+          value={word}
+          onChange={handleChange}
+        />
+        <button type="submit" className="btn btn-primary btn-sm">
           Search
         </button>
       </form>
