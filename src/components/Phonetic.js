ffb8f96d6795023c673746169d7754e79cb54b1a
Liza Neiman <5gb.place.for.love@gmail.com>
2022-08-23 16:15:10 +0300

    phonetic start

---
+++ src/components/Phonetic.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../css/Phonetic.css";
+
+function Phonetic(props) {
+  console.log(props);
+  //   for (const [key, value] of Object.entries(props.phonetic)) {
+  //     return `${key}: ${value}`;
+  //   }
+  return <ul></ul>;
+}
+export default Phonetic;
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "../css/Results.css";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 function Results(props) {
   if (props.data) {
     return (
       <div className="Results">
         <h2>{props.data.word}</h2>
+        <div>
+          {props.data.phonetics.map((phonetic, index) => {
+            return <Phonetic data={phonetic} key={index} />;
+          })}
+        </div>
         <div>
           {props.data.meanings.map((meaning, index) => {
             return <Meaning data={meaning} key={index} />;
