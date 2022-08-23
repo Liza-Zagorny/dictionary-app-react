f077609d05694d0d74f3fab40c17b9a37f94b158
Liza Neiman <5gb.place.for.love@gmail.com>
2022-08-23 15:24:38 +0300

    Added synonyms

--- src/components/Definition.js
+++ src/components/Definition.js
@@ -6,6 +6,17 @@ function Definition(props) {
     return (
       <div className="Definition">
         <p className="definition">{props.data.definition}</p>
+        {props.data.example ? (
+          <p className="example">Example: {props.data.example}</p>
+        ) : null}
+        {props.data.synonyms.length > 0 ? (
+          <p className="synonyms">
+            Synonyms:{" "}
+            {props.data.synonyms.map((synonym, index) => {
+              return <li key={index}>{synonym}</li>;
+            })}
+          </p>
+        ) : null}
       </div>
     );
   } else return null;
