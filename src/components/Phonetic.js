3211ee62f06f2f28c87901be817dc30aacf7ed45
Liza Neiman <5gb.place.for.love@gmail.com>
2022-08-25 13:19:43 +0300

    listen to phonetics

--- src/components/Phonetic.js
+++ src/components/Phonetic.js
@@ -2,10 +2,24 @@ import React from "react";
 import "../css/Phonetic.css";
 
 function Phonetic(props) {
-  console.log(props);
-  //   for (const [key, value] of Object.entries(props.phonetic)) {
-  //     return `${key}: ${value}`;
-  //   }
-  return <ul></ul>;
+  function handleListen(event) {
+    event.preventDefault();
+    window.open(props.data.audio);
+  }
+  if (props.data.audio && props.data.text && props.data.audio !== "")
+    return (
+      <span className="Phonetic">
+        <button
+          className="btn btn-primary btn-sm listen-btn"
+          onClick={handleListen}
+        >
+          <i className="bi bi-play"></i>
+        </button>
+        {props.data.text}
+      </span>
+    );
+  else if (props.data.text && (props.data.audio === "" || !props.data.audio)) {
+    return <span className="Phonetic">{props.data.text}</span>;
+  }
 }
 export default Phonetic;
